ef7f856caeceea0509ea181075c4c1a0108f8330
Lorna <c13740351@mydit.ie>
2015-09-30 15:27:08 +0000

    third

--- Lab3/palindrome.py
+++ Lab3/palindrome.py
@@ -4,3 +4,5 @@ check = raw_input("Enter a string: ")
 #reverse the input from the user
 reverse = reversed(check)
 
+#check to see if the reverse equals the original input
+if list(check) == list(reverse):
