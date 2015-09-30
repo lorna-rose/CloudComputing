08dd8ed5eb343dade5bb20d7647a4c79644d0f0a
Lorna <c13740351@mydit.ie>
2015-09-30 15:28:28 +0000

    fourth

--- Lab3/palindrome.py
+++ Lab3/palindrome.py
@@ -4,5 +4,8 @@ check = raw_input("Enter a string: ")
 #reverse the input from the user
 reverse = reversed(check)
 
-#check to see if the reverse equals the original input
+#check to see if the reverse equals the original input and if so it is a palindrome
 if list(check) == list(reverse):
+    print ("It is a palindrome")
+else:
+  print ("It is not a palindrome")
